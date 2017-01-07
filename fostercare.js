5d3e487a1987d93638e906cb64f8ab1004df8777
Asha Richardson <asha@Ashas-iMac.local>
2017-01-06 19:41:03 -0800

    add quotes around audio titles

--- fostercare.js
+++ fostercare.js
@@ -46,7 +46,7 @@ function showAll(){
 
 
    function mInfo2(){
-    document.getElementById("testing").innerHTML = "<strong>Finding Family Through Foster Care </strong> <br> <img src='Melissa.jpg' class='img-fluid'> <br> <br> <audio controls> <source src='Non-Relative Foster Family_Foster Youth Voices_Being In Foster Care Made Me Stronger_Melissa Beavers.mp3' tyle='audio/mpeg'> </audio> <br> Youth Radio reporter Melissa Beavers, 22, was in elementary school when she was placed in foster care. She says her former foster mother was the parent she always dreamed of.";
+    document.getElementById("testing").innerHTML = '<strong>"Finding Family Through Foster Care" </strong> <br> <img src="Melissa.jpg" class="img-fluid"> <br> <br> <audio controls> <source src="Non-Relative Foster Family_Foster Youth Voices_Being In Foster Care Made Me Stronger_Melissa Beavers.mp3" type="audio/mpeg"> </audio> <br> Youth Radio reporter Melissa Beavers, 22, was in elementary school when she was placed in foster care. She says her former foster mother was the parent she always dreamed of.';
 
    showAll();
     document.getElementById("Lt").innerHTML = "Foster Family - Non-Relative";
@@ -58,7 +58,7 @@ function showAll(){
 
    function mInfo3(){
    	showAll();
-    document.getElementById("testing").innerHTML = "<strong>When Home Feels Like a Prison</strong> <br> <img src='noel_hub.jpg' class='img-fluid'> <br>   <br> <audio controls> <source src='Group Home_Foster Youth Voices_When Home Feels Like A Prison_Noel Anaya.mp3' tyle='audio/mpeg'> </audio> <br> <br> Youth Radio reporter and former foster youth Noel Anaya, now 21, spent a year in a group home when he was 12 years old. He says from the moment he got there, he felt like it was a mistake.";
+    document.getElementById("testing").innerHTML = '<strong>"When Home Feels Like a Prison"</strong> <br> <img src="noel_hub.jpg" class="img-fluid"> <br>   <br> <audio controls> <source src="Group Home_Foster Youth Voices_When Home Feels Like A Prison_Noel Anaya.mp3" type="audio/mpeg"> </audio> <br> <br> Youth Radio reporter and former foster youth Noel Anaya, now 21, spent a year in a group home when he was 12 years old. He says from the moment he got there, he felt like it was a mistake.';
     document.getElementById("Lt").innerHTML = "Group Home";
     document.getElementById("Ld").innerHTML = "If you are placed in a foster group home, that means instead of living with a family, you live with up to 12 other foster kids with adults watching over you at all times. You could end up here for a number of reasons. Maybe you <a href='http://www1.uwindsor.ca/people/gorey/system/files/ChildYouthCareForum2016.pdf' target='_blank'> got in trouble with the law</a>,  or you have <a href='https://www.childwelfare.gov/topics/outofhome/group-residential-care/' target='_blank'>emotional, medical or mental health needs </a>. Group homes are usually not the first choice for foster kids, and some have been rocked by <a href='https://thinkprogress.org/how-south-carolinas-foster-care-system-is-failing-the-most-vulnerable-kids-fcf2c4d0dccf#.kcsstbk31' target='_blank'>scandals </a>. In 2016, <a href='https://www.propublica.org/documents/item/1698860-ccr-legislativereport' target='_blank'>California</a> passed legislation aiming to <a href='https://www.propublica.org/article/california-passes-group-home-legislation-foster-families-troubled-youth' target='_blank'>improve group homes and recruit more families</a> to take in foster kids.";
     document.getElementById('Li').src="Group Home.png";
@@ -75,7 +75,7 @@ function showAll(){
         document.getElementById("testing").innerHTML = " ";
 
         document.getElementById("Lt").innerHTML = "Institution";
-        document.getElementById("Ld").innerHTML = "It may sound clinical, but a foster care “institution” is pretty much a larger type of group home, for example, a treatment facility or emergency shelter.   If you are placed in a shelter, your stay may last anywhere from a <a href='http://ahum.assembly.ca.gov/sites/ahum.assembly.ca.gov/files/hearings/062811-BarthInstitutionsvFosterHomes.pdf' target='_blank'>couple of hours to several months</a>. Like group homes, institutions tend to be filled with <a href='http://ahum.assembly.ca.gov/sites/ahum.assembly.ca.gov/files/hearings/062811-BarthInstitutionsvFosterHomes.pdf' target='_blank'>older children</a>, who are harder to place with foster families.";
+        document.getElementById("Ld").innerHTML = "It sound clinical, but a foster care “institution” is pretty much a larger type of group home, for example, a treatment facility or emergency shelter.   If you are placed in a shelter, your stay may last anywhere from a <a href='http://ahum.assembly.ca.gov/sites/ahum.assembly.ca.gov/files/hearings/062811-BarthInstitutionsvFosterHomes.pdf' target='_blank'>couple of hours to several months</a>. Like group homes, institutions tend to be filled with <a href='http://ahum.assembly.ca.gov/sites/ahum.assembly.ca.gov/files/hearings/062811-BarthInstitutionsvFosterHomes.pdf' target='_blank'>older children</a>, who are harder to place with foster families.";
         document.getElementById('Li').src="shelter.png";
         document.getElementById('head2').style.visibility='visible';
   
